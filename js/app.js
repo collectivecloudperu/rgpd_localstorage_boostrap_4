dbcf0d83163c57c78c0084dd32f2deae071e02e0
Nube Colectiva <collectivecloudperu@users.noreply.github.com>
2019-07-04 19:08:37 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,11 +4,11 @@ function hbRgpd() {
   document.getElementsByClassName("rgpdbh")[0].style.display = 'none'; // Oculto mi div con nombre de clase 'rgpdbh'
 }    
 
-// Seteamos 'rgpd_nc_aceptado' en Local Storage
+// Leo si el dato 'rgpd' esta en Local Storage
 var rgpdval = localStorage.getItem("rgpd");
 //console.log(gdprval);
 
-// Verificamos si 'rgpd_nc_aceptado' ha sido seteado en Local Storage
+// Verificamos si el dato 'rgpd' ha sido seteado en Local Storage
 if (rgpdval == 'aceptado') {
   document.getElementsByClassName("rgpdbh")[0].style.display = 'none';
 }
