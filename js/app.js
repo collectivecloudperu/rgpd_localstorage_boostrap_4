87837d5f5a625fe6bcdfec5ab0547efa952525ec
Nube Colectiva <collectivecloudperu@users.noreply.github.com>
2019-07-04 19:17:24 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,9 +9,9 @@ var rgpdval = localStorage.getItem("rgpd");
 //console.log(gdprval);
 
 // Verifico si el dato 'rgpd' ha sido almacenado en localStorage
-if (rgpdval == 'aceptado') {
+if (rgpdval == 'aceptado') { // Si contiene el valor 'aceptado' ya no le mostramos la barra
   document.getElementsByClassName("rgpdbh")[0].style.display = 'none';
 }
-else {
+else { // Si no cerro la barra y no tiene almacenado el dato 'rgpd' con el valor 'aceptado' en localStorage, no se le muestra la Barra RGPD
   document.getElementsByClassName("rgpdbh")[0].style.display = 'inherit';
 } 
